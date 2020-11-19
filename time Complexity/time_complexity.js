da27f1cbf92486c68cf45dd079570d9b6eb10319
Mushhfiqur Niloy <mushfiqur@evaly.com.bd>
2020-11-19 12:19:26 +0600

    Learn Time Complexity

--- time Complexity/time_complexity.js
+++ time Complexity/time_complexity.js
@@ -1,7 +1,18 @@
-let n = 10;
-let result = 0;
+let even = []
+let n = 100;
+let input = 55;
 
-for (let i = 1; i <= n; i++) {
-    result = result + i;
+for (let i = 0; i < n; i++) {
+    even[i] = 0
 }
-console.log(result)
+for (let j = 0; j < n; j += 2) {
+    even[j] = 1
+}
+
+if (even[input]) {
+    console.log("Input is Even Number")
+} else {
+    console.log("Input Odd Number")
+}
+
+console.log(even)
